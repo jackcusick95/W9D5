c5c0ad45f4119402fdb3baf520fa96447766b5fa
jackcusick95 <jackcusick95@gmail.com>
2021-04-23 15:27:49 -0400

    added dropdown

--- skeleton 6/dist/main.js
+++ skeleton 6/dist/main.js
@@ -7,7 +7,6 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/clock.js":
@@ -16,7 +15,18 @@
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+"use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _warmup__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./warmup */ \"./src/warmup.js\");\n\n\nclass Clock {\n  constructor() {\n    const currentTime = new Date();\n    this.hours = currentTime.getHours();\n    this.minutes = currentTime.getMinutes();\n    this.seconds = currentTime.getSeconds();\n  }\n\n  printTime() {\n    const timeString = [this.hours, this.minutes, this.seconds].join(\":\");\n    return timeString;\n  }\n\n  _updateHours() {\n    this.hours += 1;\n    if (this.hours === 24) {\n      this.hours = 0;\n    }\n  }\n\n  _updateMinutes() {\n    this.minutes += 1;\n    if (this.minutes === 60) {\n      this.minutes = 0;\n      this._updateHours();\n    }\n  }\n\n  _updateSeconds() {\n    this.seconds += 1;\n    if (this.seconds === 60) {\n      this.seconds = 0;\n      this._updateMinutes();\n    }\n  }\n\n  tick() {\n    this._updateSeconds();\n    return this.printTime();\n  }\n\n}\n\nlet clockElement = document.querySelector(\"#clock\");\nlet clock = new Clock();\n\nsetInterval(() => {\n  (0,_warmup__WEBPACK_IMPORTED_MODULE_0__.htmlGenerator)(clock.tick.bind(clock)(),clockElement);\n},1000);\n\n\n\n\n\n//# sourceURL=webpack:///./src/clock.js?");
+
+/***/ }),
+
+/***/ "./src/drop_down.js":
+/*!**************************!*\
+  !*** ./src/drop_down.js ***!
+  \**************************/
+/***/ (() => {
+
+eval("\nconst dogs = {\n  \"Corgi\": \"https://www.akc.org/dog-breeds/cardigan-welsh-corgi/\",\n  \"Australian Shepherd\": \"https://www.akc.org/dog-breeds/australian-shepherd/\",\n  \"Affenpinscher\": \"https://www.akc.org/dog-breeds/affenpinscher/\",\n  \"American Staffordshire Terrier\": \"https://www.akc.org/dog-breeds/american-staffordshire-terrier/\",\n  \"Tosa\": \"https://www.akc.org/dog-breeds/tosa/\",\n  \"Labrador Retriever\": \"https://www.akc.org/dog-breeds/labrador-retriever/\",\n  \"French Bulldog\": \"https://www.akc.org/dog-breeds/french-bulldog/\" \n};\nfunction dogLinkCreator() {\n  let dogLinks = [];\n\n  for (let key in dogs) {\n\n    let a = document.createElement('a');\n    a.textContent = key;\n    a.href = dogs[key];\n    let li = document.createElement('li');\n    li.appendChild(a);\n    li.className = \"dog-link\"; \n    dogLinks.push(li); \n  } \n  return dogLinks;\n}\n\nfunction attachDogLinks() {\n  let dogLinks = dogLinkCreator();\n  let ul = document.querySelector(\".drop-down-dog-list\");\n\n  dogLinks.forEach( dog => {\n    ul.appendChild(dog); \n  })\n}\n\nattachDogLinks(); \n\n\n\n//# sourceURL=webpack:///./src/drop_down.js?");
 
 /***/ }),
 
@@ -26,7 +36,8 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _war
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _warmup__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./warmup */ \"./src/warmup.js\");\n/* harmony import */ var _clock__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./clock */ \"./src/clock.js\");\n\n\n\n\n\n//# sourceURL=webpack:///./src/index.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _warmup__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./warmup */ \"./src/warmup.js\");\n/* harmony import */ var _clock__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./clock */ \"./src/clock.js\");\n/* harmony import */ var _drop_down__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./drop_down */ \"./src/drop_down.js\");\n/* harmony import */ var _drop_down__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_drop_down__WEBPACK_IMPORTED_MODULE_2__);\n\n\n\n\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ }),
 
@@ -36,6 +47,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _war
   \***********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+"use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"htmlGenerator\": () => (/* binding */ htmlGenerator)\n/* harmony export */ });\n\nconst partyHeader = document.getElementById('party');\n\nconst htmlGenerator = (string, htmlElement) => { \n    const pChildren = htmlElement.getElementsByTagName(\"p\");\n    if (pChildren.length) {\n        for(let p of pChildren) { // could also set pChildren with Array.from to do forEach\n            htmlElement.removeChild(p);\n        }\n    }\n    const p = document.createElement(\"p\");\n    p.textContent = string;\n    htmlElement.appendChild(p);\n};\n\nhtmlGenerator('Party Time.', partyHeader);\n\n//# sourceURL=webpack:///./src/warmup.js?");
 
 /***/ })
@@ -67,6 +79,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	}
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	(() => {
 /******/ 		// define getter functions for harmony exports
--- skeleton 6/src/drop_down.js
+++ skeleton 6/src/drop_down.js
@@ -8,4 +8,30 @@ const dogs = {
   "Labrador Retriever": "https://www.akc.org/dog-breeds/labrador-retriever/",
   "French Bulldog": "https://www.akc.org/dog-breeds/french-bulldog/" 
 };
+function dogLinkCreator() {
+  let dogLinks = [];
+
+  for (let key in dogs) {
+
+    let a = document.createElement('a');
+    a.textContent = key;
+    a.href = dogs[key];
+    let li = document.createElement('li');
+    li.appendChild(a);
+    li.className = "dog-link"; 
+    dogLinks.push(li); 
+  } 
+  return dogLinks;
+}
+
+function attachDogLinks() {
+  let dogLinks = dogLinkCreator();
+  let ul = document.querySelector(".drop-down-dog-list");
+
+  dogLinks.forEach( dog => {
+    ul.appendChild(dog); 
+  })
+}
+
+attachDogLinks(); 
 
